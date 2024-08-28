e7029584e25747a5a42ac6774d41cd58124e4dda
fuwx <f2928560492@163.com>
2024-08-28 11:51:37 +0800

    add ci

--- agent/java/check_download.sh
+++ agent/java/check_download.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # 下载探针文件到指定目录
-mkdir -p v2.5.0
-if [ ! -f v2.5.0/opentelemetry-javaagent.jar ]; then
-    curl -o v2.5.0/opentelemetry-javaagent.jar -L \
+mkdir -p opentelemetry/v2.5.0
+if [ ! -f opentelemetry/v2.5.0/opentelemetry-javaagent.jar ]; then
+    curl -o opentelemetry/v2.5.0/opentelemetry-javaagent.jar -L \
         https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.5.0/opentelemetry-javaagent.jar
+fi
+
+mkdir -p skywalking/
+if [ ! -f skywalking/v9.3.0/skywalking-agent.jar ]; then
+    curl -O https://dlcdn.apache.org/skywalking/java-agent/9.3.0/apache-skywalking-java-agent-9.3.0.tgz
+    tar -zxvf apache-skywalking-java-agent-9.3.0.tgz -C skywalking
+    mv skywalking/skywalking-agent skywalking/v9.3.0
+    rm apache-skywalking-java-agent-9.3.0.tgz
 fi
